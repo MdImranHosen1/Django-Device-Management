b782a8c41852ecafbb23bfd93105e31197fe28dd
Md Imran Hosen <62979868+MdImranHosen1@users.noreply.github.com>
2024-01-28 15:35:16 +0600

    Added comment

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,13 +1,17 @@
 from django.db import models
-
+# If we need than we can add more information about an company. For simplicity add only name.
 class Company(models.Model):
     name = models.CharField(max_length=255,unique=True)
-
+    
+    
+# mployee model has 3 varible.Name,Contact_information,company. Company is foreignKey which indicate the Company model.
 class Employee(models.Model):
     name = models.CharField(max_length=255)
     contact_information = models.CharField(max_length=255)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
 
+
+#In this model i take the history of every taken devices.
 class Checkout(models.Model):
     device_name = models.CharField(max_length=255)
     CONDITION_CHOICES = [
